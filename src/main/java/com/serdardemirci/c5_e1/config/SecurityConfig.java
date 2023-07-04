9291b89b59fc92425cb9cd2bb8cbc9ff2972f4fc
Serdar Demirci <o_serdardemirci@hotmail.com>
2023-07-04 10:19:14 +0200

    test

--- src/main/java/com/serdardemirci/c5_e1/config/SecurityConfig.java
+++ src/main/java/com/serdardemirci/c5_e1/config/SecurityConfig.java
@@ -1,2 +1,61 @@
-package com.serdardemirci.c5_e1.config;public class SecurityConfig {
+package com.serdardemirci.c5_e1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .httpBasic()
+        .and()
+        .authorizeRequests()
+//        .anyRequest().authenticated()
+//        .anyRequest().permitAll()
+//        .anyRequest().denyAll()
+//        .anyRequest().hasAuthority("read")
+//        .anyRequest().hasAnyAuthority("read", "ROLE_ADMIN")
+//        .anyRequest().hasRole("ADMIN")
+        .anyRequest().access("isAuthenticated() and hasAuthority('read') and hasRole('ROLE_ADMIN')")
+        .and()
+        .build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    var userDetailsService = new InMemoryUserDetailsManager();
+
+    var user1 = User
+        .withUsername("serdar")
+        .password(passwordEncoder().encode("12345"))
+        .authorities("read")
+        .roles("ADMIN")
+        .build();
+
+    var user2 = User
+        .withUsername("armin")
+        .password(passwordEncoder().encode("12345"))
+        .authorities("read")
+        .roles("USER")
+        .build();
+
+    userDetailsService.createUser(user1);
+    userDetailsService.createUser(user2);
+
+    return userDetailsService;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
--- src/main/java/com/serdardemirci/c5_e1/controller/DemoController.java
+++ src/main/java/com/serdardemirci/c5_e1/controller/DemoController.java
@@ -1,2 +1,13 @@
-package com.serdardemirci.c5_e1.controller;public class DemoController {
+package com.serdardemirci.c5_e1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+  @GetMapping("/demo")
+  public String demo() {
+    return "demo";
+  }
 }
